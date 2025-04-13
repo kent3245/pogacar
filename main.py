3f01be797976f9cf84eb71323db326332cf65071
kent3245 <1126kentziti@gmail.com>
2025-04-12 21:46:17 -0700

    fdsjkj

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 # Import the necessary class from procyclingstats
 from procyclingstats import Rider
+import json
 
 # Create an instance for a specific rider
 rider = Rider("rider/tadej-pogacar")
@@ -9,4 +10,6 @@ print("Rider Birthdate:", rider.birthdate())
 
 # Alternatively, you can parse all available data
 rider_data = rider.parse()
-print("Rider Data:", rider_data)
+print(json.dumps(rider_data, indent=4))
+
+# print("Rider Data:", rider_data)
