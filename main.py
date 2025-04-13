6b9f88b9f5bf4d0f74f52241b5ef89320699d993
kent3245 <1126kentziti@gmail.com>
2025-04-12 23:05:53 -0700

    ff

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 # Import the necessary class from procyclingstats
 from procyclingstats import Rider
+from procyclingstats import Race
+from procyclingstats import Team
 import json
 
 # Create an instance for a specific rider
@@ -13,3 +15,6 @@ rider_data = rider.parse()
 print(json.dumps(rider_data, indent=4))
 
 # print("Rider Data:", rider_data)
+
+# race = Race("race/tour-de-france/2022")
+# print(json.dumps(race.parse(),indent=4))
